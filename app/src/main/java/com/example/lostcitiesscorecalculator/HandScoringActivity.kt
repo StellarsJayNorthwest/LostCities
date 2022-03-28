fb2599525a95cc25f3e25f61eb7601d1efec382f
Jeff Westhead <jeff314159@hotmail.com>
2022-03-27 19:06:28 -0700

    Hooking up activities and layout tweaks

--- app/src/main/java/com/example/lostcitiesscorecalculator/HandScoringActivity.kt
+++ app/src/main/java/com/example/lostcitiesscorecalculator/HandScoringActivity.kt
@@ -1,5 +1,6 @@
 package com.example.lostcitiesscorecalculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.lostcitiesscorecalculator.databinding.HandScoringActivityBinding
@@ -11,5 +12,9 @@ class HandScoringActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = HandScoringActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.player1Blue.setOnClickListener {
+            val intent = Intent(this, SingleColorScoringActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
